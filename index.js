a4033a1b451f27ea8a073b5889d7981c9d64e8b4
Heather MacPhail <heather@hmacphail.com>
2021-06-27 15:31:02 -0700

    Angular app init

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// database connection =============================
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -10,22 +11,25 @@ const pool = new Pool({
   }
 });
 
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// routes =============================================
+app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/', (req, res) => res.render('pages/index'));
+// app.get('/db', async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query('SELECT * FROM test_table');
+//     const results = { 'results': (result) ? result.rows : null};
+//     res.render('pages/db', results );
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Error " + err);
+//   }
+// });
+
+
+// app.use('/api', require('./server/routes'));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
