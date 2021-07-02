c8ea0944cd20d713a5350f6a47b392a33e41d243
Heather MacPhail <heather@hmacphail.com>
2021-07-01 23:56:46 -0700

    Angular app build

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const path = require('path')
+const express = require("express")
+const compression = require("compression")
+const path = require("path")
 const PORT = process.env.PORT || 5000
 
 const app = express();
 
 // static pages
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(express.static(path.join(__dirname, "public")));
 
 // api routes
-app.use('/api', require('./server/routes'));
+app.use("/api", require("./server/routes"));
 
 // start app on PORT
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
