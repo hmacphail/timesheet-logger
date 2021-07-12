0a2c7e0f85a7952347134e4a86ca21aaea136eec
Heather MacPhail <heather@hmacphail.com>
2021-07-12 00:14:59 -0700

    Discount tables and calculation

--- src/app/components/month-summary/month-summary.component.ts
+++ src/app/components/month-summary/month-summary.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { DateTime } from "luxon";
 import { NgxUiLoaderService } from "ngx-ui-loader";
 
+import { DefaultRent, RentDiscount, WeeklyDiscountTiers } from "src/app/models/discount";
 import { TimeEntryService } from "src/app/services/time-entry.service";
 import { ITimeEntry } from "src/app/models/time-entry";
 
@@ -14,7 +15,8 @@ export class MonthSummaryComponent implements OnInit {
   allTimeEntries: ITimeEntry[] = [];
   visibleTimeEntries: ITimeEntry[] = [];
 
-  selectedMonth: number = DateTime.now().month;
+  readonly NOW = DateTime.now();
+  selectedMonth: number = this.NOW.month;
   months = [
     { value: 1, label: "Jan" },
     { value: 2, label: "Feb" },
@@ -30,6 +32,15 @@ export class MonthSummaryComponent implements OnInit {
     { value: 12, label: "Dec" },
   ];
 
+  readonly WEEKS_PER_MNTH = 4.2;
+
+  discountTableEntries: IDiscountTableEntry[] = [];
+
+  currentHrsPerWk: number = 0;
+  currentHrsPerMnth: number = 0;
+  currentDiscount: number = 0;
+  currentRent: number = DefaultRent;
+
   constructor(
     private timeEntryService: TimeEntryService,
     private loaderService: NgxUiLoaderService
@@ -38,7 +49,10 @@ export class MonthSummaryComponent implements OnInit {
   ngOnInit(): void {
     this.loaderService.start();
     this.getTimeEntries();
+    this.setupDiscountTable();
   }
+
+  // ---------- PAGE SETUP -------------
 
   getTimeEntries(): void {
     this.timeEntryService.getAllTimeEntries().subscribe((entries) => {
@@ -55,11 +69,32 @@ export class MonthSummaryComponent implements OnInit {
     });
 
     // filter by selected month
-    this.setMonthFilter(this.allTimeEntries);
+    this.setMonthFilter();
 
     // complete page load animation
     this.loaderService.stop();
   }
+
+  setupDiscountTable(): void {
+    for (const wkTier in WeeklyDiscountTiers) {
+      const weekTier: number = Number.parseInt(wkTier);
+      if (!isNaN(weekTier)) {
+        const rentDiscount = RentDiscount[wkTier];
+        const isExtraTier = (weekTier == WeeklyDiscountTiers.Six || weekTier == WeeklyDiscountTiers.Seven
+          || weekTier == WeeklyDiscountTiers.Eight || weekTier == WeeklyDiscountTiers.Nine);
+
+        this.discountTableEntries.push({
+          discount: rentDiscount,
+          hrsPerWk: weekTier,
+          hrsPerMnth: Math.round(weekTier * this.WEEKS_PER_MNTH),
+          rent: DefaultRent - rentDiscount,
+          isExtraTier: isExtraTier
+        });
+      }
+    }
+  }
+
+  // -------- INTERFACE ACTIONS ---------
 
   getDuration(timeEntry: ITimeEntry): string {
     const start = DateTime.fromISO(timeEntry.startTime as string);
@@ -74,14 +109,62 @@ export class MonthSummaryComponent implements OnInit {
     return `${paddedMins}m`;
   }
 
-  setMonthFilter(allEntries?: ITimeEntry[]): void {
-    if (!allEntries) {
-      allEntries = this.allTimeEntries;
-    }
-    this.visibleTimeEntries = allEntries.filter((te) => {
+  setMonthFilter(): void {
+    // filter entries table to selected month
+    this.visibleTimeEntries = this.allTimeEntries.filter((te) => {
       const dateTime = DateTime.fromISO(te.startTime as string);
-      return dateTime.month == this.selectedMonth && dateTime.year == DateTime.now().year;
+      return dateTime.month == this.selectedMonth && dateTime.year == this.NOW.year;
     });
+
+    // re-calculate discount for selected month
+    this.calculateMonthlyData();
   }
 
+  private calculateMonthlyData(): void {
+    // reset data
+    this.currentHrsPerMnth = 0;
+    this.currentHrsPerWk = 0;
+    this.currentDiscount = 0;
+    this.currentRent = DefaultRent;
+
+    const thisWeekNumber = this.NOW.weekNumber;
+
+    // iterate through filtered time entries
+    for (const monthEntry of this.visibleTimeEntries) {
+      const startDateTime = DateTime.fromISO(monthEntry.startTime as string);
+      const endDateTime = DateTime.fromISO(monthEntry.endTime as string);
+      const duration = endDateTime.diff(startDateTime, "hours");
+
+      this.currentHrsPerMnth += duration.hours;
+      if (startDateTime.weekNumber == thisWeekNumber) {
+        this.currentHrsPerWk += duration.hours;
+      }
+    }
+
+    // calculate rent and discount
+    for (const wkTier in WeeklyDiscountTiers) {
+      const weekTier: number = Number.parseInt(wkTier);
+      if (!isNaN(weekTier)) {
+        const mnthTier = weekTier * this.WEEKS_PER_MNTH;
+
+        // if monthly hours is greater than tier, tier has been hit, continue
+        if (this.currentHrsPerMnth >= mnthTier) {
+          this.currentDiscount = RentDiscount[wkTier];
+        }
+        else {
+          break;
+        }
+      }
+    }
+    this.currentRent = DefaultRent - this.currentDiscount;
+  }
+
+}
+
+interface IDiscountTableEntry {
+  discount: number;
+  rent: number;
+  hrsPerWk: number;
+  hrsPerMnth: Number;
+  isExtraTier: boolean;
 }
